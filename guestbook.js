dfeeb0bdca29416a22bd49926e3398e30dfb94e2
crocus13 <66542067+crocus13@users.noreply.github.com>
2022-02-28 15:50:43 -0800

    Update guestbook.js
    Added API url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://a4e2b954.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
